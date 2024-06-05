3c6c00a25ec2b6b59851917616b069dbd7dc3dfc
garciaradaelli <garciaradaelliruben@gmail.com>
2024-06-05 10:06:15 +0200

    Add files via upload

---
+++ src/main/java/Ruben/CoheteLuna/CoheteLunaApplication.java
@@ -0,0 +1,13 @@
+package Ruben.CoheteLuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoheteLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoheteLunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ruben/CoheteLuna/CoheteLunaApplicationTests.java
@@ -0,0 +1,13 @@
+package Ruben.CoheteLuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoheteLunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
